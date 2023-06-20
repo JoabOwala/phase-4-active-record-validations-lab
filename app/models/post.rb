8726385fd13009bb2b35e33b584ca35dad2570c4
JoabOwala <owalajoab@gmail.com>
2023-06-20 23:51:41 +0300

    Assignment Completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum:250 }
+    validates :summary, length: { maximum:250 }
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
+    
+    validate :title_not_clickbait
+
+    private
+
+     def title_not_clickbait
+         if title == "True Facts"
+           errors.add(:title, "cannot be 'True Facts' (clickbait)")
+         end
+     end
 end
